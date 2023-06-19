e55d830ab6de8d572ae0a5a968e5370949af1790
ooi18 <48976740+ooi18@users.noreply.github.com>
2023-06-19 08:52:05 +0800

    Add files via upload

---
+++ docs/source/conf.py
@@ -0,0 +1,77 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'IP2Location'
+copyright = '2023, IP2Location'
+author = 'IP2Location'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    # 'myst_parser',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+# html_theme = 'sphinx_rtd_theme'
+# html_theme = 'press'
+# html_theme = 'sphinx_material'
+html_theme = 'sphinx_book_theme' #Sphinx ver 6.2.1
+# html_theme = 'furo'
+# html_theme = 'renku'
+
+'''
+# Material theme options (see theme.conf for more information)
+html_theme_options = {
+
+    # Set the name of the project to appear in the navigation.
+    'nav_title': 'MailboxValidator',
+
+    # Set you GA account ID to enable tracking
+    # 'google_analytics_account': 'UA-XXXXX',
+
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    'base_url': 'https://github.com/ooi18/mailboxvalidator-python',
+
+    # Set the color and the accent color
+    'color_primary': 'blue',
+    'color_accent': 'light-blue',
+
+    # Set the repo location to get a badge with stats
+    # 'repo_url': 'https://github.com/ooi18/mailboxvalidator-python',
+    'repo_url': 'https://github.com/mailboxvalidator/mailboxvalidator-python',
+    'repo_name': 'Project',
+
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': 3,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+}'''
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = 'images/ipl-logo-square-1200.png'
+
+html_title = "IP2Location Python"
---
+++ docs/source/quickstart.rst
@@ -0,0 +1,22 @@
+Quickstart
+==========
+
+Requirements
+------------
+
+1. Python 3.5 and above.
+
+Installation
+------------
+
+1. Unzip the package.
+2. Execute python setup.py build
+3. Execute python setup.py install
+
+or
+
+To install this module type the following (for PyPI):
+
+```bash
+pip install IP2Location
+```
